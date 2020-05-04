a23f3cf605eb01bcd72ec880c8a3a9a646df77a1
Bryce B <bryce.boddy@gmail.com>
2020-05-04 04:24:19 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -13,7 +13,8 @@ class Triangle
   #end
   
   def kind
-    if side1 == side2 && side1 == side3
+    if (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 + side3) <= side2
+    elsif side1 == side2 && side1 == side3
       :equilateral
     elsif side1 == side2 || side1 == side3 || side2 == side3
       :isosceles
