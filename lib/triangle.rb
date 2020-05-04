8900dd19d5df194130feae61e39a10c2fe783fb2
Bryce B <bryce.boddy@gmail.com>
2020-05-04 04:27:15 +0000

    Automatically backed up by Learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -7,11 +7,6 @@ class Triangle
     @side3 = side3
   end
   
- #def valid
-    #raise TriangleError if side1 <= 0 && side2 <= 0 && side3 <= 0
-    #raise TriangleError if side1 + side2 > side3 && side1 + side3 > side2 && side2 + side3 > side1
-  #end
-  
   def kind
     if (side1 + side2) <= side3 || (side2 + side3) <= side1 || (side1 + side3) <= side2
       raise TriangleError
